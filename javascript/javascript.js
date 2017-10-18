53a5dcf7122f5a3481273ede9b9a34d23c383f1f
Bruno Almeida <brunoalmeida356@gmail.com>
2017-10-18 12:41:15 -0300

    alterações na responsividade e menu

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,6 +1,47 @@
-// 
+//
 // $(document).ready(function(){
 //     var altura = $(window).height();
 //
 //     alert(altura);
+// });
+
+$(document).ready(function(){
+    var owl = $('.owl-carousel');
+  owl.owlCarousel({
+      items:1,
+      loop:true,
+      margin:10,
+      autoplay:true,
+      autoplayTimeout:2500,
+      autoplayHoverPause:true
+  });
+  $('.play').on('click',function(){
+      owl.trigger('play.owl.autoplay',[1000])
+  })
+  $('.stop').on('click',function(){
+      owl.trigger('stop.owl.autoplay')
+  })
+});
+
+// $('.owl.carousel').owlCarousel({
+//     center: true,
+//     items:2,
+//     loop:true,
+//     margin:10,
+//     responsive:{
+//         600:{
+//             items:4
+//         }
+//     }
+// });
+// $('.nonloop').owlCarousel({
+//     center: true,
+//     items:2,
+//     loop:false,
+//     margin:10,
+//     responsive:{
+//         600:{
+//             items:4
+//         }
+//     }
 // });
